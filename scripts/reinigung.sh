cb25f3ae3c74aeed14bd1ccec89f7623d5b71e13
bubi-bubi <nova.jost@outlook.com>
2025-11-25 15:43:08 +0100

    sed-Befehle reinigung.sh

--- scripts/reinigung.sh
+++ scripts/reinigung.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "Hello World"
-echo "Hell"
+sed 's/Année/jahr/' |
+sed 's/Mois/monat/' |
+sed 's/Semaine/woche/' |
+sed 's/Date début semaine/wstart/' |
+sed 's/Femmes 0-64/wjung/' |
+sed 's/Femmes 65+/walt/' |
+sed 's/Hommes 0-64/mjung/' |
+sed 's/Hommes 65+/malt/' |
+sed 's/Total/total/';
