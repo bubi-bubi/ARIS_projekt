6727b84ddaac9c20ac77f01f8d4c3e6a44b2e478
heinigshaila <shaila.heiniger@stud.fhgr.ch>
2025-11-26 16:07:44 +0100

    filterskript

---
+++ scripts/test1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Content-type: text/html"
+echo ""
+
+echo "<h1>Gefilterte Ergebnisse</h1>
+
+CSV="/var/www/html/14_03_deces_hebdomadaires.csv"
+Gender=$(echo "$QERY_STRING" | SED -n 's/^.*gender=\([^&]*\).*$/\1/p' | sed "s/%20/ /g")
+
+echo "<ul>"
+tail -n +2 "$CSV" | while IFS=, read -r col1 col2 col3
+do
+if [[ "$GENDER" == "alle" || "$col2" == "$GENDER"]
+echo "<li>$col1, $col2, $col3, $col4, $col5, $col6</li>"
+fi
+done
+echo "</ul>"
+echo"a href='/index.html'>Zurück zum Filter</a>"
+echo"</body></html>"
