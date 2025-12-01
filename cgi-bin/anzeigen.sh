49366005353287bb3c331d65fd64ac37f43d5e90
bubi-bubi <nova.jost@outlook.com>
2025-12-01 15:41:41 +0100

    anzeigen.sh skript erstellt/kopiert

---
+++ cgi-bin/anzeigen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+DATEI_PFAD="../data/todesfälle.csv"
+
+echo "Content-type: text/html"
+echo ""
+echo "<html><head>"
+echo "<link rel=\"stylesheet\" href=\"../css/style.css\">"
+echo "<title>Todesfälle </title>"
+echo "</head>"
+echo "<body>"
+echo "<header> <h1>Todesfälle</h1> </header>"
+echo "<section>"
+echo "<p>"
+cat "$DATEI_PFAD" | sed 's/$/<br>/'
+echo "</p></section>"
+echo "<section><p>Zurück zur <a href="../index.html">Haupseite.</a></p></section>"
+echo "<footer><p>&copy; Todesfälle Freiburg</p> </footer>"
+echo "</body></html>"
