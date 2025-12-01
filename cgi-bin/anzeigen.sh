fafb70b16925b6ca7a116c736dd9ca02e0c85d2f
bubi-bubi <nova.jost@outlook.com>
2025-12-01 16:41:29 +0100

    anpassung anzeigen.sh

--- cgi-bin/anzeigen.sh
+++ cgi-bin/anzeigen.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8
 
 DATEI_PFAD="../data/encoded-todesfälle.csv"
 
-echo "Content-type: text/html"
+echo "Content-type: text/html; charset=UTF-8"
 echo ""
 echo "<html><head>"
 echo "<link rel=\"stylesheet\" href=\"../css/style.css\">"
@@ -16,6 +16,6 @@ echo "<section>"
 echo "<p>"
 cat "$DATEI_PFAD" | sed 's/$/<br>/'
 echo "</p></section>"
-echo "<section><p>Zurück zur <a href="../index.html">Haupseite.</a></p></section>"
+echo "<section><p>Zurück zur <a href=\"../index.html\">Haupseite.</a></p></section>"
 echo "<footer><p>&copy; Todesfälle Freiburg</p> </footer>"
 echo "</body></html>"
