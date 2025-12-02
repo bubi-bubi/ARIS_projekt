00c6bfed7525e9272aff9a241a69112724ce26c0
heinigshaila <shaila.heiniger@stud.fhgr.ch>
2025-12-02 11:47:43 +0100

    Paginierung next back

--- cgi-bin/test1.sh
+++ cgi-bin/test1.sh
@@ -54,43 +54,4 @@ NR>1 {
 }' "$DATEI_PFAD")
 
 # Anzahl gefilterte Zeilen
-TOTAL_LINES=$(echo "$FILTERED_DATA" | wc -l)
-TOTAL_PAGES=$(( (TOTAL_LINES + PER_PAGE - 1) / PER_PAGE ))
-START=$(( (PAGE-1)*PER_PAGE + 1 ))
-END=$(( START + PER_PAGE - 1 ))
-
-# Ausgabe Datenzeilen
-echo "$FILTERED_DATA" | awk -F';' -v start=$START -v end=$END '{
-    row_num=NR
-    if(row_num>=start && row_num<=end){
-        print "<tr>";
-        for(i=1;i<=NF;i++) print "<td>" $i "</td>";
-        print "</tr>"
-    }
-}'
-
-echo "</table>"
-
-# Paginierung Links
-if [ "$TOTAL_PAGES" -gt 1 ]; then
-    echo "<div style='margin-top:20px;'>"
-    echo "<p>Seiten: "
-    for i in $(seq 1 $TOTAL_PAGES); do
-        if [ "$i" -eq "$PAGE" ]; then
-            echo "<strong>$i</strong> "
-        else
-            LINK="test1.sh?page=$i"
-            [ -n "$FILTER" ] && LINK="${LINK}&filter=$FILTER"
-            [ -n "$YEAR" ] && LINK="${LINK}&year=$YEAR"
-            echo "<a href='$LINK'>$i</a> "
-        fi
-    done
-    echo "</p>"
-    echo "</div>"
-fi
-
-# Footer
-echo "<section><p>Zurück zur <a href=\"../testindex.html\">Auswahl</a>.</p></section>"
-echo "<footer><p>&copy; Todesfälle Freiburg</p></footer>"
-echo "</body></html>"
-
+TOTAL_LINES=$(echo "$F_
