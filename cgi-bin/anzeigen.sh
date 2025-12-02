99eb660fdf470e6c3b986a27505261b9889f7587
heinigshaila <shaila.heiniger@stud.fhgr.ch>
2025-12-02 08:54:16 +0100

    Tabellierung

--- cgi-bin/anzeigen.sh
+++ cgi-bin/anzeigen.sh
@@ -2,47 +2,63 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+# Pfad zur CSV-Datei
 DATEI_PFAD="../data/encoded-todesfälle.csv"
-#es muss noch die Paginierung ermöglicht werden!
 
+# Konfiguration
+PER_PAGE=20
+
+# GET-Parameter (page)
+PAGE=$(echo "$QUERY_STRING" | sed -n 's/^page=\([0-9]*\)$/\1/p')
+PAGE=${PAGE:-1}
+
+# Berechnung der Zeilen
+START=$(( (PAGE-1)*PER_PAGE + 1 ))
+END=$(( START + PER_PAGE - 1 ))
+
+# HTML HEADER
 echo "Content-type: text/html; charset=UTF-8"
 echo ""
-#TEST FÜR PAGINIERUNG----------------------------
-#konfiguration
-DATEI_PFAD="../data/encoded-todesfälle.csv"
-PER_PAGE=20
-# GET-Parameter auslesen (z. B. ?page=2)
-PAGE=$(echo "$QUERY_STRING" | sed -n 's/^page=\([0-9]*\)$/\1/p')
-PAGE=${PAGE:-1}  # Standard: Seite 1
-# Berechne Start- und Endzeile
-START=$(( (PAGE-1)*PER_PAGE + 1 ))  # erste Zeile für diese Seite
-END=$(( START + PER_PAGE - 1 ))
-#TEST FÜR PAGINIERUNG----------------------------
-
 echo "<html><head>"
 echo "<link rel=\"stylesheet\" href=\"../css/style.css\">"
-echo "<title>Todesfälle </title>"
-echo "</head>"
-echo "<body>"
-echo "<header> <h1>Todesfälle</h1> </header>"
+echo "<title>Todesfälle</title>"
+echo "</head><body>"
+echo "<header><h1>Todesfälle</h1></header>"
 
-#TEST FÜR PAGINIERUNG----------------------------
-# Kopfzeile der CSV anzeigen
-head -n 1 "$DATEI_PFAD" | while read line; do
-    echo "<p><strong>$line</strong></p>"
-done
+##############################################
+#           CSV ALS TABELLE
+##############################################
 
-# Daten für die aktuelle Seite anzeigen
-tail -n +2 "$DATEI_PFAD" | sed -n "${START},${END}p" | while read line; do
-    echo "<p>$line</p>"
-done
+echo "<table border='1' cellpadding='6' cellspacing='0'>"
 
-# Paging-Links
+# Kopfzeile (Header)
+head -n 1 "$DATEI_PFAD" | awk -F';' '{
+    print "<tr>";
+    for(i=1;i<=NF;i++) print "<th>" $i "</th>";
+    print "</tr>";
+}'
+
+# Datenzeilen
+tail -n +2 "$DATEI_PFAD" | sed -n "${START},${END}p" | \
+awk -F';' '{
+    print "<tr>";
+    for(i=1;i<=NF;i++) print "<td>" $i "</td>";
+    print "</tr>";
+}'
+
+echo "</table>"
+
+##############################################
+#             PAGINIERUNG
+##############################################
+
 TOTAL_LINES=$(wc -l < "$DATEI_PFAD")
-DATA_LINES=$((TOTAL_LINES-1))  # ohne Kopfzeile
-TOTAL_PAGES=$(( (DATA_LINES + PER_PAGE - 1)/PER_PAGE ))
+DATA_LINES=$((TOTAL_LINES-1))
+TOTAL_PAGES=$(( (DATA_LINES + PER_PAGE - 1) / PER_PAGE ))
 
+echo "<div style='margin-top:20px;'>"
 echo "<p>Seiten: "
+
 for i in $(seq 1 $TOTAL_PAGES); do
     if [ "$i" -eq "$PAGE" ]; then
         echo "<strong>$i</strong> "
@@ -50,13 +66,14 @@ for i in $(seq 1 $TOTAL_PAGES); do
         echo "<a href='anzeigen.sh?page=$i'>$i</a> "
     fi
 done
-echo "</p>"
-#TEST FÜR PAGINIERUNG----------------------------
 
-#echo "<section>"
-#echo "<p>"
-#cat "$DATEI_PFAD" | sed 's/$/<br>/'
-#echo "</p></section>"
-echo "<section><p>Zurück zur <a href=\"../index.html\">Haupseite.</a></p></section>"
-echo "<footer><p>&copy; Todesfälle Freiburg</p> </footer>"
+echo "</p>"
+echo "</div>"
+
+##############################################
+#            FUSS / FOOTER
+##############################################
+
+echo "<section><p>Zurück zur <a href=\"../index.html\">Hauptseite</a>.</p></section>"
+echo "<footer><p>&copy; Todesfälle Freiburg</p></footer>"
 echo "</body></html>"
