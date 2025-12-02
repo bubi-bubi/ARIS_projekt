e8bff419e72280e0e10ef8b8ab08941702e9d941
heinigshaila <shaila.heiniger@stud.fhgr.ch>
2025-12-02 11:14:57 +0100

    paginierung korrektur

--- cgi-bin/test1.sh
+++ cgi-bin/test1.sh
@@ -5,16 +5,15 @@ export LANG=en_US.UTF-8
 echo "Content-type: text/html; charset=UTF-8"
 echo ""
 
+# Pfadanagabe
 DATEI_PFAD="../data/encoded-todesfälle.csv"
 
 # Paginierung
 PER_PAGE=20
 PAGE=$(echo "$QUERY_STRING" | tr '&' '\n' | grep '^page=' | cut -d'=' -f2)
 PAGE=${PAGE:-1}
-START=$(( (PAGE-1)*PER_PAGE + 1 ))
-END=$(( START + PER_PAGE - 1 ))
 
-# Filter und Jahr auslesen
+# Filter: Gender & Jahr
 FILTER=$(echo "$QUERY_STRING" | tr '&' '\n' | grep '^filter=' | cut -d'=' -f2)
 YEAR=$(echo "$QUERY_STRING" | tr '&' '\n' | grep '^year=' | cut -d'=' -f2)
 
@@ -25,10 +24,8 @@ echo "<title>Todesfälle</title>"
 echo "</head><body class=\"anzeige\">"
 echo "<header><h1>Todesfälle Freiburg</h1></header>"
 
-# Tabelle
+# Tabelle Header
 echo "<table class=\"anzeige-table\" border='1' cellpadding='6' cellspacing='0'>"
-
-# Kopfzeile
 head -n 1 "$DATEI_PFAD" | awk -F';' -v filter="$FILTER" '{
     print "<tr>";
     if (filter=="frauen") {
@@ -41,18 +38,31 @@ head -n 1 "$DATEI_PFAD" | awk -F';' -v filter="$FILTER" '{
     print "</tr>"
 }'
 
-# Datenzeilen
-tail -n +2 "$DATEI_PFAD" | sed -n "${START},${END}p" | \
-awk -F';' -v filter="$FILTER" -v year="$YEAR" '{
-    if(year!="" && $1!=year) next  # Jahr filtern
-
+# Gefilterte Daten
+FILTERED_DATA=$(awk -F';' -v filter="$FILTER" -v year="$YEAR" '
+NR>1 {
+    if(year!="" && $1!=year) next
     if(filter=="frauen") {
-        sum=$5+$6; # Frauen 0-64 + 65+
-        print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td><td>"sum"</td><td>"$9"</td></tr>"
+        sum=$5+$6
+        print $1 ";" $2 ";" $3 ";" $4 ";" sum ";" $9
     } else if(filter=="maenner") {
-        sum=$7+$8; # Männer 0-64 + 65+
-        print "<tr><td>"$1"</td><td>"$2"</td><td>"$3"</td><td>"$4"</td><td>"sum"</td><td>"$9"</td></tr>"
+        sum=$7+$8
+        print $1 ";" $2 ";" $3 ";" $4 ";" sum ";" $9
     } else {
+        print $0
+    }
+}' "$DATEI_PFAD")
+
+# Anzahl gefilterte Zeilen
+TOTAL_LINES=$(echo "$FILTERED_DATA" | wc -l)
+TOTAL_PAGES=$(( (TOTAL_LINES + PER_PAGE - 1) / PER_PAGE ))
+START=$(( (PAGE-1)*PER_PAGE + 1 ))
+END=$(( START + PER_PAGE - 1 ))
+
+# Ausgabe Datenzeilen
+echo "$FILTERED_DATA" | awk -F';' -v start=$START -v end=$END '{
+    row_num=NR
+    if(row_num>=start && row_num<=end){
         print "<tr>";
         for(i=1;i<=NF;i++) print "<td>" $i "</td>";
         print "</tr>"
@@ -61,28 +71,25 @@ awk -F';' -v filter="$FILTER" -v year="$YEAR" '{
 
 echo "</table>"
 
-# Paginierung
-TOTAL_LINES=$(wc -l < "$DATEI_PFAD")
-DATA_LINES=$((TOTAL_LINES-1))
-TOTAL_PAGES=$(( (DATA_LINES + PER_PAGE - 1) / PER_PAGE ))
-
-echo "<div style='margin-top:20px;'>"
-echo "<p>Seiten: "
-for i in $(seq 1 $TOTAL_PAGES); do
-    if [ "$i" -eq "$PAGE" ]; then
-        echo "<strong>$i</strong> "
-    else
-        LINK="test1.sh?page=$i"
-        [ -n "$FILTER" ] && LINK="$LINK&filter=$FILTER"
-        [ -n "$YEAR" ] && LINK="$LINK&year=$YEAR"
-        echo "<a href='$LINK'>$i</a> "
-    fi
-done
-echo "</p>"
-echo "</div>"
+# Paginierung Links
+if [ "$TOTAL_PAGES" -gt 1 ]; then
+    echo "<div style='margin-top:20px;'>"
+    echo "<p>Seiten: "
+    for i in $(seq 1 $TOTAL_PAGES); do
+        if [ "$i" -eq "$PAGE" ]; then
+            echo "<strong>$i</strong> "
+        else
+            LINK="test1.sh?page=$i"
+            [ -n "$FILTER" ] && LINK="${LINK}&filter=$FILTER"
+            [ -n "$YEAR" ] && LINK="${LINK}&year=$YEAR"
+            echo "<a href='$LINK'>$i</a> "
+        fi
+    done
+    echo "</p>"
+    echo "</div>"
+fi
 
 # Footer
 echo "<section><p>Zurück zur <a href=\"../testindex.html\">Auswahl</a>.</p></section>"
 echo "<footer><p>&copy; Todesfälle Freiburg</p></footer>"
 echo "</body></html>"
-
